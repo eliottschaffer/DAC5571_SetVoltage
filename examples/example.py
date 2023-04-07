9de1e491c611528a5a6f94e278dcc7a39d532921
eliottschaffer <92275246+eliottschaffer@users.noreply.github.com>
2023-04-07 16:27:03 -0700

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,6 @@
-from setVoltageFunction import set_voltage
+from setVoltage import setVoltage
 
 #This command sets the voltage to 0 Volts with a VDD of 3.3 at the DAC5571 with an address 0x4d
 
-set_voltage(0.0, 3.3, 0x4d)
+setVoltage(0.0, 3.3, 0x4d)
 
