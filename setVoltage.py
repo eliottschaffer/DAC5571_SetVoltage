8acb5a87f2347102f178614df25e708a269ab11b
zhurli <98350812+zhurli@users.noreply.github.com>
2023-04-06 18:37:37 -0700

    Update setVoltage.py

--- setVoltage.py
+++ setVoltage.py
@@ -1,43 +1,44 @@
 import subprocess
 
-#Set your VDD Here
-
+# Set the maximum voltage value (VDD)
 VDD = 3.3
 
-
-#Set the DAC5571 Address, Either 0x4d or 0x4c
-
+# Set the I2C address of the DAC5571 (either 0x4d or 0x4c)
 address = "0x4d"
 
+# Loop indefinitely to continuously prompt the user for a desired voltage
 while(True):
 
-
+    # Initialize the flag variable and voltage variable
     bool_value = True
     voltage = 0
 
+    # Prompt the user for a voltage value until a valid value is entered
     while (bool_value):
-            voltagein = input("Enter Voltage 0-"+ str(VDD)+  ": \n")
+        voltagein = input("Enter Voltage 0-"+ str(VDD)+  ": \n type 'exit', 'quit', or 'q' to terminate the program")
 
-            if (voltagein == "exit" or voltagein == "quit" or voltagein == "q"):
-                exit()
+        # Allow the user to exit the loop by typing 'exit', 'quit', or 'q'
+        if (voltagein == "exit" or voltagein == "quit" or voltagein == "q"):
+            exit()
 
-            try:
-                attempt = float(voltagein)
-            except ValueError:
-                print("Wrong Value Type")
-                continue
+        try:
+            attempt = float(voltagein)
+        except ValueError:
+            # Print an error message if the entered value is not a valid number
+            print("Wrong Value Type")
+            continue
 
-            voltage = float(voltagein)
+        # Convert the input voltage to a float and validate that it falls within the acceptable range
+        voltage = float(voltagein)
+        if (voltage <= VDD and voltage >= 0 ):
+            bool_value = False
 
-            if (voltage <= VDD and voltage >= 0 ):
-                bool_value = False
-
+    # Normalize the voltage value and convert it to a hexadecimal string
     normalizedvoltage = int(voltage / VDD * 4095)
-
-
 
     HexVoltage =  hex(normalizedvoltage)
 
+    # Convert the hexadecimal string to a list of characters, extract major and minor digits, and construct the I2C compatible command to set the DAC
     hexlist = list(HexVoltage)
 
     length = len(hexlist)
@@ -47,11 +48,12 @@ while(True):
 
     bashCommand = "i2cset -y 1 "+address+" 0x0" + majordigit + " 0x" + minordigit
 
-
+    # If the hexadecimal string is 4 characters long, set the minor digit to the last two characters
     if length == 4:
         minordigit = ''.join(hexlist[2:4])
         bashCommand = "i2cset -y 1 "+address+" 0x00 0x" + minordigit
 
+    # If the hexadecimal string is 3 characters long, set the minor digit to the last character
     if length == 3:
         minordigit = hexlist[2]
         bashCommand = "i2cset -y 1 "+address+" 0x00 0x0" + minordigit
@@ -59,14 +61,15 @@ while(True):
     print(bashCommand)
 
     try:
+        # Execute the I2C command
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()\
+        output, error = process.communicate()
 
     except subprocess.CalledProcessError as e:
+        # Print an error message if there is an error executing the I2C command
         print("Error executing command: ", e)
         continue
 
+    # Print a message
 
-    print("Voltage Propertly Set\n--------------------------------")
 
-    continue
